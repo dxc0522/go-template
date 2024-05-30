5d8b279ffc9d448232c8d4a3060670e2676c7bb1
doudou <809685229@qq.com>
2024-05-30 09:05:35 +0800

    add unauthorized error code

--- app/app.go
+++ app/app.go
@@ -3,6 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/go-template/common/response"
+	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"net/http"
 
 	"github.com/go-template/app/internal/config"
 	"github.com/go-template/app/internal/handler"
@@ -20,7 +24,7 @@ func main() {
 	var c config.Config
 	conf.MustLoad(*configFile, &c)
 
-	server := rest.MustNewServer(c.RestConf)
+	server := rest.MustNewServer(c.RestConf, rest.WithUnauthorizedCallback(JwtUnauthorizedResult))
 	defer server.Stop()
 
 	ctx := svc.NewServiceContext(c)
@@ -28,4 +32,10 @@ func main() {
 
 	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
 	server.Start()
+}
+
+// JwtUnauthorizedResult jwt验证失败的回调
+func JwtUnauthorizedResult(w http.ResponseWriter, r *http.Request, err error) {
+	logx.Info("jwt unauthorized", err)
+	httpx.WriteJson(w, http.StatusUnauthorized, response.Body{http.StatusUnauthorized, http.StatusText(http.StatusUnauthorized), nil})
 }
